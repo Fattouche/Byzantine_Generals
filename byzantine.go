ecea630b71c12c82ae1a90cab8c9cd133ae43cb8
fattouche <alex.fattouche@gmail.com>
2018-11-06 17:13:44 -0800

    Finalize algorithm

--- byzantine.go
+++ byzantine.go
@@ -7,27 +7,40 @@ import (
 	"time"
 )
 
+const (
+	ATTACK  = "ATTACK"
+	RETREAT = "RETREAT"
+)
+
 // The flag to specify the number of generals
-var G = flag.Int("G", 4, "The number of generals. First is commander, rest are lieutenants")
+var G = flag.Int("G", 7, "The number of generals. First is commander, rest are lieutenants")
 
 // The flag to specify how many faulty generals there are
-var M = flag.Int("M", 1, "The number of faulty generals")
+var M = flag.Int("M", 2, "The number of faulty generals")
 
 // The flag to specify the commany that will be given by the commander(general 0)
 var O = flag.String("O", "ATTACK", "The order by the general(ATTACK or RETREAT)")
 
+// Values:
+//	inputValue(string): The order the node has been given
+//	outputValue(string): The order the node decides on
+//	processIDs(map[int]int): Keeps track of which nodes have seen this message before
+//	children([]*Node): Keeps track of the children of the node
+//	id(int): The id of the corresponding general
 type Node struct {
 	inputValue  string
 	outputValue string
-	process_ids map[int]int
+	processIDs  map[int]int
 	children    []*Node
+	id          int
 }
 
-type General struct {
-	id   int
-	node *Node
-}
+// Parameters:
+//		numFaultyGenerals(int): The number of faulty generals
+//		numGenerals(int): The total number of generals
 
+// This function will randomly select numFaultyGenerals generals to be the faulty ones.
+// The commander can also be chosen.
 func genFaultyIndexes(numFaultyGenerals, numGenerals int) map[int]int {
 	faultyIndexes := make(map[int]int)
 	rand.Seed(time.Now().UnixNano())
@@ -38,64 +51,150 @@ func genFaultyIndexes(numFaultyGenerals, numGenerals int) map[int]int {
 	return faultyIndexes
 }
 
-func initializeGenerals(numGenerals int, order string) []General {
-	generals := make([]General, numGenerals)
-	for i := 0; i < numGenerals; i++ {
-		generals[i] = General{i, nil}
-	}
-}
+// Paramters:
+//		order(string): The order that will be give
 
+// This function returns the opposite of the given order.
 func oppositeOrder(order string) string {
-	if order == "RETREAT" {
-		return "ATTACK"
+	if order == RETREAT {
+		return ATTACK
 	} else {
-		return "RETREAT"
+		return RETREAT
 	}
 }
 
-func (general General) broadcastOrder(generals []General, faultyGenerals map[int]int) {
+// Parameters:
+//		id(int): The id of the node you want to message
+//		faultyGenerals(map[int]int): A map of all the generals that are known to be faulty.
+
+// This function will simulate sending a message by updating the information of the recieving node.
+// It will then add the recieivng node as one of its children which will be used later in the decision.
+func (node Node) sendMessage(id int, faultyGenerals map[int]int) *Node {
+	var order string
+	if _, ok := node.processIDs[id]; ok {
+		return nil
+	}
 	faultyGeneral := false
-	if _, ok := faultyGenerals[0]; ok {
+	if _, ok := faultyGenerals[node.id]; ok {
 		faultyGeneral = true
 	}
-	for i := 0; i < len(generals); i++ {
-		if generals[i].node == nil {
-			generals[i].node = new(Node)
-		}
-		if generals[i].node.process_ids[general.id] == 1 {
-			continue
-		}
-		if i%2 == 0 && faultyGeneral {
-			generals[i].node.inputValue = oppositeOrder(general.node.inputValue)
-		} else {
-			generals[i].node.inputValue = general.node.inputValue
-		}
-		generals[i].node.process_ids[general.id] = 1
-		general.node.children = append(general.node.children, generals[i].node)
+	if id%2 == 0 && faultyGeneral {
+		order = oppositeOrder(node.inputValue)
+	} else {
+		order = node.inputValue
 	}
+	processIDs := make(map[int]int)
+	for k, v := range node.processIDs {
+		processIDs[k] = v
+	}
+	processIDs[id] = 1
+	return &Node{order, "", processIDs, nil, id}
+}
+
+// This is responsible for deciding the decision of each of the nodes within our created tree.
+func (node *Node) decide() string {
+	// Base case: If this is a leaf node, then the output value is the input value
+	if len(node.children) == 0 {
+		node.outputValue = node.inputValue
+		return node.outputValue
+	}
+	// If it isn't a leaf node it means we have children that we need to use to decide.
+	decisions := make(map[string]int)
+	for _, child := range node.children {
+		decision := child.decide()
+		if _, ok := decisions[decision]; ok {
+			decisions[decision]++
+		} else {
+			decisions[decision] = 1
+		}
+	}
+	// Get the majority vote from this nodes children and assign it to the output value.
+	node.outputValue = majorityDecision(decisions)
+	return node.outputValue
+}
+
+// Parameters:
+//		decisions(map[string]int): A map of the count of each ATTACK or RETREAT decisions
+
+// This helper function is used to return the majority decision among a nodes children.
+func majorityDecision(decisions map[string]int) string {
+	if decisions[ATTACK] > decisions[RETREAT] {
+		return ATTACK
+	} else {
+		return RETREAT
+	}
+}
+
+// Parameters:
+//		numGenerals(int): The number of generals(including commander)
+//		numFaultyGenerals(int): The number of faulty generals
+//		order(string): The order that the commander will give to his generals
+
+// This is the main orchestrator for the byzantine generals algorithm
+func byzantineGenerals(numGenerals, numFaultyGenerals int, order string) {
+	//Randomly choose which generals are faulty(can include the commander)
+	faultyIndexes := genFaultyIndexes(numFaultyGenerals, numGenerals)
+
+	// Initialize the commander with the original order and itself as a visited commander in the map.
+	// This will ensure the commander is never messaged again.
+	commander := &Node{order, "", map[int]int{0: 1}, nil, 0}
+	if faultyIndexes[0] == 1 {
+		fmt.Println("Commander is faulty")
+	}
+	queue := []*Node{commander}
+
+	currDepth := 0
+	elemDepth := 1
+	nextElemDepth := 0
+
+	// This is a depth-limited breadth first search so that we sendMessages and create nodes for all generals,
+	// only up to the depth defined as the number of faulty generals.
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		nextElemDepth += numGenerals
+		elemDepth--
+		if elemDepth == 0 {
+			currDepth++
+			if currDepth > numFaultyGenerals {
+				break
+			}
+			elemDepth = nextElemDepth
+			nextElemDepth = 0
+		}
+		// Iterate through each general and send the message, add to end of BFS queue.
+		for i := 1; i < numGenerals; i++ {
+			childNode := node.sendMessage(i, faultyIndexes)
+			if childNode == nil {
+				continue
+			}
+			node.children = append(node.children, childNode)
+			queue = append(queue, childNode)
+		}
+	}
+	// We have built the tree, now we can decide what to the consensus decision is.
+	commander.outputValue = commander.decide()
+
+	for i, general := range commander.children {
+		if _, ok := faultyIndexes[i+1]; ok {
+			fmt.Print("Faulty ")
+		}
+		fmt.Printf("general %d decides on %s\n", general.id, general.outputValue)
+	}
+	fmt.Printf("Consensus decision of %s", commander.outputValue)
 }
 
 func main() {
 	flag.Parse()
+	// Parse flags in to better variable names
 	var numGenerals int = *G
 	var numFaultyGenerals int = *M
 	var order string = *O
+	// Make sure that the number of faulty generals is less than a third of the number of generals
 	if 3*numFaultyGenerals > numGenerals {
 		fmt.Println("Too many faulty generals, can only have a third of the total generals being faulty")
 		return
 	}
-	//Randomly choose which generals are faulty
-	faultyIndexes := genFaultyIndexes(numFaultyGenerals, numGenerals)
-	//Initialize the generals
-	generals := initializeGenerals(numGenerals, order)
-	generals[0].node = &Node{order, "", map[int]int{0: 1}, nil}
-	//Using the commander, initialize the broadcast to all other generals
-	generals[0].broadcastOrder(generals, faultyIndexes)
-
-	for i := 1; i <= numFaultyGenerals; i++ {
-		for j := 1; j < numGenerals; j++ {
-			node := generals[j].node.children
-			generals[j].broadcastOrder(generals)
-		}
-	}
+	// Call the orchestrator
+	byzantineGenerals(numGenerals, numFaultyGenerals, order)
 }
